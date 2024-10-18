d92d6d3e5c689fbffb163856d708172732df77f1
azizbekrakhimjonov <azizbekrahimjonov571@gmail.com>
2024-10-18 15:17:01 +0500

    first commit

---
+++ project/asgi.py
@@ -0,0 +1,27 @@
+# import os
+#
+# from django.core.asgi import get_asgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+#
+# application = get_asgi_application()
+
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from .routing import websocket_urlpatterns
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        ),
+    }
+)
